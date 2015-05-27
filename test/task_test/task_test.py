42d58c037e732503b033d9957d6d1d08e6afe976
Yugeng Hui <interhuiyg@163.com>
2015-05-28 01:21:26 +0800

    更新测试用例

--- test/task_test/task_test.py
+++ test/task_test/task_test.py
@@ -2,8 +2,6 @@
 
 import time
 
-from task.trigger import simple_trigger
-from task.trigger import cron_trigger
 from task import task
 from task import task_container
 
@@ -11,35 +9,46 @@ import my_job
 
 container = task_container.TaskContainer()
 
-_job = my_job.MyJob()
+from task.trigger import simple_trigger
+def test_simple_trigger():
+    _job = my_job.MyJob()
+    
+    _trigger = simple_trigger.SimpleTrigger(50, 3)
+    _trigger.set_delay(5, 0)
+    _task = task.Task('Task', _job, _trigger)
+    
+    container.add_task(_task)
+    
+    print '---------Start Simple Trigger---------'
+    container.start_all()
+    time.sleep(10)
+    
+    print '---------Pause---------'
+    container.pasuse_all()
+    time.sleep(30)
+    
+    print '---------Restart---------'
+    container.start_all()
+    time.sleep(60)
+    
+    print '---------Stop---------'
+    container.stop_all()
+    time.sleep(10)
+    
+    print '---------Remove---------'
+    container.remove_task('Task')
 
-_trigger = simple_trigger.SimpleTrigger(50, 3)
-_trigger.set_delay(5, 0)
-_task = task.Task('Task', _job, _trigger)
+from task.trigger import cron_trigger
+def test_cron_trigger():
+    _job = my_job.MyJob()
+    _trigger = cron_trigger.CronTrigger('0-59/5 10,15,20 * * MAY * 2015')
+    _task = task.Task('Task2', _job, _trigger)
+    
+    container.add_task(_task)
 
-container.add_task(_task)
-
-container.start_all()
-
-time.sleep(10)
-
-container.pasuse_all()
-
+    print '---------Start Cron Trigger---------'
+    container.start_all()
+    
+test_simple_trigger()
 time.sleep(30)
-
-container.start_all()
-
-time.sleep(60)
-
-container.stop_all()
-
-time.sleep(10)
-
-container.remove_task('Task')
-
-_trigger = cron_trigger.CronTrigger('0-59/5 48,50,52 * 28 MAY * 2015')
-_task = task.Task('Task2', _job, _trigger)
-
-container.add_task(_task)
-
-container.start_all()
+test_cron_trigger()
